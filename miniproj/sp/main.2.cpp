d060ec17422ee7e4ee5bc67fb664a060f54dadbd
Ken Hasselmann <kennes@weegee.fr>
2011-12-13 22:50:37 +0100

    version avec rapport

--- miniproj/sp/main.2.cpp
+++ miniproj/sp/main.2.cpp
@@ -6,42 +6,45 @@ int main()
 {
     int n(0), nbelt, nb;
     matrice *m1(0);
-    matrice *m2(0);
-    m1 = new matrice;
+    matrice *m2(0); //on creer des pointeurs vers des matrices
+    m1 = new matrice; //on aloue la memoire des matrices
     m2 = new matrice;
 
-    initialise(m1);
+    initialise(m1); //on initialise les nombres d'elements dans les lignes Ã  zero
     initialise(m2);
 
-    cout << "Combien de nb à classer ?" << endl;
+    cout << "Combien de nb Ã  classer ?" << endl;
     cin >> nbelt;
     cout << "Entrez ces nombres :" << endl;
     for (int i(0);i<nbelt;i++)
     {
         cin >> nb;
-        insert(nb, m1, 0); // elt, tableau, indice de tri
-		while(nb > pow(10,n)) n++;
+        insert(nb, m1, 0); 
+// on met l'element nb dans la matrice m1, indice 0
+		while(nb > pow(10,n)) n++; 
+//on determine le nombres d'etapes a faire
     }
-    afficher(m1);
+    afficher(m1); //on affiche la matrice
     cout << "Il y aura " << n << " etapes" << endl;
 
     for (int j(1);j<n;j++)
     {
-        sort(m1, m2, j);
+        sort(m1, m2, j); //on tri 
         matrice* tmp = m1;
-        m1 = m2;
+        m1 = m2; //on echange les pointeurs des 2 matrices
         m2 = tmp;
         initialise(m2);
+ //on remet les nombres d'elements Ã  0 dans la matrice d'arrivÃ©e
     }
     cout << "Resultat :" << endl;
     afficher(m1);
-    delete m1;    //On libère la case mémoire
+    delete m1;    //On libÃ¨re la case mÃ©moire
     m1 = 0;
-    delete m2;    //On libère la case mémoire
+    delete m2;    //On libÃ¨re la case mÃ©moire
     m2 = 0;
 }
 
-void initialise(matrice* mat)
+void initialise(matrice* mat) //initialisation des matrices
 {
     for (int i(0);i<10;i++)
     {
@@ -49,7 +52,7 @@ void initialise(matrice* mat)
     }
 }
 
-void afficher(matrice* mat)
+void afficher(matrice* mat) //fonction d'affichage
 {
     for (int i(0);i<10;i++)
     {
@@ -58,18 +61,19 @@ void afficher(matrice* mat)
         {
             cout << (*mat).tab[i][j] << " ";
         }
-        cout << "(" << (*mat).elt[i] << " elts)" <<endl;// | Classe " << i << ")"<< endl;
+        cout << "(" << (*mat).elt[i] << " elts)" <<endl;
     }
 }
 
-void insert(int nb, matrice* mat, int etape)
+void insert(int nb, matrice* mat, int etape) 
+//fonction d'insertion des nombres dans une matrice suivant une etape
 {
     int indice = (int)floor(nb/pow(10,etape))%10;
     (*mat).tab[indice][(*mat).elt[indice]] = nb;
     (*mat).elt[indice]++;
 }
 
-void sort(matrice *in, matrice *out, int etape)
+void sort(matrice *in, matrice *out, int etape) //on tri la matrice d'entree in dans la matrice de sortie out suivant l'etape
 {
     for (int i(0);i<10;i++)
     {
