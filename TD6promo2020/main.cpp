9b6b9952ca16acb8eb3d231b3875e5f9b9c92e6e
Ken <ken@ken-notebook.(none)>
2012-01-10 23:24:15 +0100

    td6 cpp fini creation liste debut menu

--- TD6promo2020/main.cpp
+++ TD6promo2020/main.cpp
@@ -24,20 +24,45 @@ int main()
 	fichier = fopen("Promo.txt", "r");
 
 	if (fichier != NULL)
+	{
+		int i = 0;
+		while(*classe[i-1]->nom != 0)
 		{
-			for (int i=0; i<10, i++)
-			{
 			classe[i] = enregistrer(fichier);
-			
-			cout <<  classe[i]->notes[i] <<endl;
-			//TODO : boucle pour creer les eleves
-
-			fclose(fichier);
+			if (*classe[i]->nom == NULL) break;
+			cout << classe[i]->nom;
+			cout << classe[i]->prenom << endl;
+			cout << classe[i]->promo << endl;
+			for (int j=0; j<9; j++)
+			{
+				cout <<  classe[i]->notes[j] <<endl;
+			}
+			i++;
 		}
+		cout << i << endl;
+
+		fclose(fichier);
+	}
 	else cout << "Erreur à l'ouverture du fichier" <<endl;
-	return 0;
 
 	//TODO : menu
+	int nb;
+	while (nb != 666)
+	{
+		cout << "tapez 1 pour la liste des eleves, 2 pour rechercher un eleve, 3 pour la moyenne d'une matiere, 4 pour ..., 666 pour quitter" << endl;
+		cin >> nb;
+		cout << endl;
+		switch (nb)
+		{
+			case 1 :
+			{
+			}
+			case 2 :
+			{
+			}
+		}
+	}
+	return 0;
 }
 
 eleve* enregistrer(FILE* fichier)
@@ -49,6 +74,5 @@ eleve* enregistrer(FILE* fichier)
 	{
 		fscanf(fichier,"%f",&nouveau->notes[i]);
 	}
-
 	return nouveau;
 }
